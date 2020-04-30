75f9707606df229723e73ec161fdbc7faa8cb915
mornir <jeromepott@hotmail.com>
2020-04-30 10:27:11 +0200

    Run cypress without plugin

--- tests/components/test.js
+++ tests/components/test.js
@@ -1,9 +1,9 @@
-import { mount } from "cypress-vue-unit-test";
-import HelloWorld from "../../src/components/HelloWorld.vue";
+import { mount } from 'cypress-vue-unit-test'
+import HelloWorld from '../../src/components/HelloWorld.vue'
 
-describe("HelloWorld", () => {
-  it("Works awesomely", () => {
-    mount(HelloWorld);
-    cy.get("[data-testid=hello-world-title]").should("exist");
-  });
-});
+describe('HelloWorld', () => {
+  it('Works awesomely', () => {
+    mount(HelloWorld)
+    cy.get('[data-testid=hello-world-title]').should('exist')
+  })
+})
