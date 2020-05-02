9a4a469ca2320b2eb2980915b9e6b0199e0be00a
mornir <jeromepott@hotmail.com>
2020-05-02 20:44:21 +0200

    Fix test

--- tests/plugins/index.js
+++ tests/plugins/index.js
@@ -1,7 +1,9 @@
 // default webpack file preprocessor is good for simple cases
 // Required to temporarily patch async components, chunking, and inline image loading
-import { onFileDefaultPreprocessor } from 'cypress-vue-unit-test/dist/preprocessor/webpack'
+const {
+  onFileDefaultPreprocessor,
+} = require('cypress-vue-unit-test/dist/preprocessor/webpack')
 
-module.exports = on => {
+module.exports = (on) => {
   on('file:preprocessor', onFileDefaultPreprocessor)
 }
