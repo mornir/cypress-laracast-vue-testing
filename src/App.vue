b69f675f9e120f1f8292dce4afa17bb9a4f9f211
mornir <jeromepott@hotmail.com>
2020-05-02 20:50:13 +0200

    Add Tailwind

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 </template>
 
 <script>
+import '@/assets/css/tailwind.css'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
@@ -16,13 +17,4 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {},
+  variants: {},
+  plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: ['./public/**/*.html', './src/**/*.vue'],
+    options: {
+      whitelistPatterns: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+      ],
+    },
+  },
+}
