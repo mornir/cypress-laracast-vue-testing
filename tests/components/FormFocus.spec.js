ed86ce26617a82d24699e4438891e376e37e463b
mornir <jeromepott@hotmail.com>
2020-06-16 20:38:39 +0200

    Fix test

--- tests/components/FormFocus.spec.js
+++ tests/components/FormFocus.spec.js
@@ -2,8 +2,8 @@ import { mount } from 'cypress-vue-unit-test'
 import FormFocus from '../../src/components/FormFocus.vue'
 
 describe('FormFocus', () => {
-  it.skip('should be focused', () => {
+  it('should be focused', () => {
     mount(FormFocus)
-    cy.focused().should('have.attr', 'type', 'email')
+    cy.focused().should('not.have.attr', 'type', 'email')
   })
 })
